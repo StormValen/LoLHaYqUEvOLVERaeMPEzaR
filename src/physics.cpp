9c0889162591e78ee0229a4d80176932bc7fdbdd
vgutierrez <vgutierrez@S5-09>
2017-02-22 13:03:50 +0100

    Waterfall and fountain working on GUI

--- src/physics.cpp
+++ src/physics.cpp
@@ -8,7 +8,9 @@
 #include <stdlib.h>
 #include <time.h>
 
-bool show_test_window = false;
+bool waterfall = true;
+bool check;
+bool show_test_window = true;
 void GUI() {
 	{	//FrameRate
 		ImGui::Text("Application average %.3f ms/frame (%.1f FPS)", 1000.0f / ImGui::GetIO().Framerate, ImGui::GetIO().Framerate);
@@ -18,7 +20,13 @@ void GUI() {
 	// ImGui test window. Most of the sample code is in ImGui::ShowTestWindow()
 	if(show_test_window) {
 		ImGui::SetNextWindowPos(ImVec2(650, 20), ImGuiSetCond_FirstUseEver);
-		ImGui::ShowTestWindow(&show_test_window);
+		check = ImGui::SmallButton("Waterfall/Fountain");
+
+
+		if (check) {
+			waterfall = !waterfall;
+		}
+
 	}
 }
 
@@ -52,28 +60,45 @@ void PhysicsInit() {
 	
 	//init position particles
 	for (int i = 0; i < LilSpheres::maxParticles; ++i) {
-		partVerts[i * 3 + 0] = 0;
-		partVerts[i * 3 + 1] = 1;
-		partVerts[i * 3 + 2] = 0;
+		if (waterfall) {
+			partVerts[i * 3 + 0] = 5;
+			partVerts[i * 3 + 1] = 8 + ((float)rand() / RAND_MAX) * 0.2f;
+			partVerts[i * 3 + 2] = ((float)rand() / RAND_MAX) * 6.f - 3.f;
+		}
+		else if (waterfall == false) {
+			partVerts[i * 3 + 0] = 0;
+			partVerts[i * 3 + 1] = 1;
+			partVerts[i * 3 + 2] = 0;
+		}
+		
 	}
 
 	//sets de initial position, velocity and life
 	srand(time(NULL));
 	for (int i = 0; i < LilSpheres::maxParticles; i++) {
-		particlesContainer[i].pos = glm::vec3(partVerts[i*3], partVerts[i*3 + 1], partVerts[i*3 + 2]);
-		particlesContainer[i].vel = glm::vec3(((float)rand() / RAND_MAX) * 6.f - 3.f,(10 +((float)rand() / RAND_MAX) * 6.f - 3.f), ((float)rand() / RAND_MAX) * 6.f - 3.f); //random
-		particlesContainer[i].life = 0;
+		if (waterfall) {
+			particlesContainer[i].pos = glm::vec3(partVerts[i * 3], partVerts[i * 3 + 1], partVerts[i * 3 + 2]);
+			particlesContainer[i].vel = glm::vec3(-3, 0, ((float)rand() / RAND_MAX) * 6.f - 3.f); //random
+			particlesContainer[i].life = 0;
+		}
+		else if (waterfall == false) {
+			particlesContainer[i].pos = glm::vec3(partVerts[i * 3], partVerts[i * 3 + 1], partVerts[i * 3 + 2]);
+			particlesContainer[i].vel = glm::vec3(((float)rand() / RAND_MAX) * 6.f - 3.f,(10 +((float)rand() / RAND_MAX) * 6.f - 3.f), ((float)rand() / RAND_MAX) * 6.f - 3.f); //random
+			particlesContainer[i].life = 0;
+		}
+		
 	}
 }
 void PhysicsUpdate(float dt) {
-	
+
+
 	//for all particles
 	for (int i = 0; i < updateRange; i++) {
 
 		//save last velocity module
 		particlesContainer[i].lastVel = particlesContainer[i].vel;
 
-		
+
 		//update vector velocity velocity with formula
 		particlesContainer[i].vel = particlesContainer[i].lastVel + gravity * timePerFrame;
 
@@ -81,16 +106,23 @@ void PhysicsUpdate(float dt) {
 		particlesContainer[i].lastPos = particlesContainer[i].pos;
 
 		//update position with formula
-		particlesContainer[i].pos = particlesContainer[i].lastPos + timePerFrame * particlesContainer[i].lastVel +  0.5f * gravity * (pow(timePerFrame, 2)); //components x and z have 0 gravity.
+		particlesContainer[i].pos = particlesContainer[i].lastPos + timePerFrame * particlesContainer[i].lastVel + 0.5f * gravity * (pow(timePerFrame, 2)); //components x and z have 0 gravity.
 
 		//life manager
 		if (particlesContainer[i].life < maxLife) {
 			particlesContainer[i].life += 1;
 		}
 		else { //init the particle (center and new random vector)
-			particlesContainer[i].pos = glm::vec3(0, 1, 0);
+			if (waterfall) {
+				particlesContainer[i].pos = glm::vec3(5, 8 + ((float)rand() / RAND_MAX) * 0.2f, ((float)rand() / RAND_MAX) * 6.f - 3.f);
+				particlesContainer[i].vel = glm::vec3(-3, 0, ((float)rand() / RAND_MAX) * 6.f - 3.f); //random
+				particlesContainer[i].life = 0;
+			}
+			else if (waterfall == false) {
+			particlesContainer[i].pos = glm::vec3(0,1,0);
 			particlesContainer[i].vel = glm::vec3(((float)rand() / RAND_MAX) * 6.f - 3.f, (10 + ((float)rand() / RAND_MAX) * 6.f - 3.f), ((float)rand() / RAND_MAX) * 6.f - 3.f); //random
 			particlesContainer[i].life = 0;
+			}
 		}
 
 		/*
