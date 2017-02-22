4ec9e747c65610f22970e0ba3c4182070d82852f
Marc Segarra Sanahuja <marcssegarra@gmail.com>
2017-02-22 20:43:36 +0100

    Hud: gravity and coefficient (slider)

--- src/physics.cpp
+++ src/physics.cpp
@@ -13,6 +13,17 @@ float radius = 0.05f;
 bool waterfall = true;
 bool check;
 bool show_test_window = true;
+
+float *partVerts;
+
+int updateRange = 20;
+float timePerFrame = 0.033;
+int maxLife = 120;
+glm::vec3 gravity = { 0, -9.8, 0 };
+glm::vec3 normal = { 0,0,0 };
+int d;
+float coef = 0.9f;
+
 void GUI() {
 	{	//FrameRate
 		ImGui::Text("Application average %.3f ms/frame (%.1f FPS)", 1000.0f / ImGui::GetIO().Framerate, ImGui::GetIO().Framerate);
@@ -24,11 +35,11 @@ void GUI() {
 		ImGui::SetNextWindowPos(ImVec2(650, 20), ImGuiSetCond_FirstUseEver);
 		check = ImGui::SmallButton("Waterfall/Fountain");
 
-
 		if (check) {
 			waterfall = !waterfall;
 		}
-
+		ImGui::SliderFloat("Gravity", &gravity.y, -15, 15);
+		ImGui::SliderFloat("Bounce Coef", &coef, 0, 1);
 	}
 }
 
@@ -39,13 +50,6 @@ namespace LilSpheres {
 	extern void updateParticles(int startIdx, int count, float* array_data);
 	extern void drawParticles(int startIdx, int count);
 }
-
-float *partVerts;
-glm::vec3 gravity = {0, -4, 0};
-int updateRange = 20;
-float timePerFrame = 0.033;
-int maxLife = 200;
-
 
 struct Particle {
 	glm::vec3 pos;
@@ -68,7 +72,7 @@ void PhysicsInit() {
 			partVerts[i * 3 + 1] = 8 + ((float)rand() / RAND_MAX) * 0.2f;
 			partVerts[i * 3 + 2] = ((float)rand() / RAND_MAX) * 6.f - 3.f;
 		}
-		else if (waterfall == false) {
+		else if (!waterfall) {
 			partVerts[i * 3 + 0] = 0;
 			partVerts[i * 3 + 1] = 1;
 			partVerts[i * 3 + 2] = 0;
@@ -84,7 +88,7 @@ void PhysicsInit() {
 			particlesContainer[i].vel = glm::vec3(-3, 0, ((float)rand() / RAND_MAX) * 6.f - 3.f); //random
 			particlesContainer[i].life = 0;
 		}
-		else if (waterfall == false) {
+		else if (!waterfall) {
 			particlesContainer[i].pos = glm::vec3(partVerts[i * 3], partVerts[i * 3 + 1], partVerts[i * 3 + 2]);
 			particlesContainer[i].vel = glm::vec3(((float)rand() / RAND_MAX) * 6.f - 3.f,(10 +((float)rand() / RAND_MAX) * 6.f - 3.f), ((float)rand() / RAND_MAX) * 6.f - 3.f); //random
 			particlesContainer[i].life = 0;
@@ -112,51 +116,51 @@ void PhysicsUpdate(float dt) {
 		particlesContainer[i].pos = particlesContainer[i].lastPos + timePerFrame * particlesContainer[i].lastVel + 0.5f * gravity * (pow(timePerFrame, 2)); //components x and z have 0 gravity.
 
 
-		//AKA VAN LAS COLISIONES
-		int d;
-		float coef = 0.5;
+		//colisions
+
+		//left wall
 		if (particlesContainer[i].pos.x <= -5 + radius) {
-			glm::vec3 normal = { 1,0,0 };
+			normal = { 1,0,0 };
 			d = 5;
-			particlesContainer[i].pos = particlesContainer[i].pos - (1+coef) * (glm::dot(normal, particlesContainer[i].pos)+d)*normal;
+			particlesContainer[i].pos = particlesContainer[i].pos - (1+coef) * (glm::dot(normal, particlesContainer[i].pos) + d)*normal;
 			particlesContainer[i].vel = particlesContainer[i].vel - (1+coef) * (glm::dot(normal, particlesContainer[i].vel))*normal;
 
 		}
-
+		//right wall
 		if (particlesContainer[i].pos.x >= 5 - radius) {
-			glm::vec3 normal = { -1,0,0 };
-			d = -5;
-			particlesContainer[i].pos = particlesContainer[i].pos - (1 + coef) * (glm::dot(normal, particlesContainer[i].pos) + d)*normal;
-			particlesContainer[i].vel = particlesContainer[i].vel - (1 + coef) * (glm::dot(normal, particlesContainer[i].vel))*normal;
-
-		}
-
-		if (particlesContainer[i].pos.z <= -5 + radius) {
-			glm::vec3 normal = { 0,0,1 };
+			normal = { -1,0,0 };
 			d = 5;
 			particlesContainer[i].pos = particlesContainer[i].pos - (1 + coef) * (glm::dot(normal, particlesContainer[i].pos) + d)*normal;
 			particlesContainer[i].vel = particlesContainer[i].vel - (1 + coef) * (glm::dot(normal, particlesContainer[i].vel))*normal;
 
 		}
-
-		if (particlesContainer[i].pos.z >= 5 - radius) {
-			glm::vec3 normal = { 0,0,-1 };
-			d = -5;
+		//front wall
+		if (particlesContainer[i].pos.z <= -5 + radius) {
+			normal = { 0,0,1 };
+			d = 5;
 			particlesContainer[i].pos = particlesContainer[i].pos - (1 + coef) * (glm::dot(normal, particlesContainer[i].pos) + d)*normal;
 			particlesContainer[i].vel = particlesContainer[i].vel - (1 + coef) * (glm::dot(normal, particlesContainer[i].vel))*normal;
 
 		}
+		//back wall
+		if (particlesContainer[i].pos.z >= 5 - radius) {
+			normal = { 0,0,-1 };
+			d = 5;
+			particlesContainer[i].pos = particlesContainer[i].pos - (1 + coef) * (glm::dot(normal, particlesContainer[i].pos) + d)*normal;
+			particlesContainer[i].vel = particlesContainer[i].vel - (1 + coef) * (glm::dot(normal, particlesContainer[i].vel))*normal;
 
+		}
+		//floor
 		if (particlesContainer[i].pos.y <= 0 + radius) {
-			glm::vec3 normal = { 0,1,0 };
+			normal = { 0,1,0 };
 			d = 0;
 			particlesContainer[i].pos = particlesContainer[i].pos - (1 + coef) * (glm::dot(normal, particlesContainer[i].pos) + d)*normal;
 			particlesContainer[i].vel = particlesContainer[i].vel - (1 + coef) * (glm::dot(normal, particlesContainer[i].vel))*normal;
 
 		}
-
+		//top wall
 		if (particlesContainer[i].pos.y >= 10 - radius) {
-			glm::vec3 normal = { 0,-1,0 };
+			normal = { 0,-1,0 };
 			d = 10;
 			particlesContainer[i].pos = particlesContainer[i].pos - (1 + coef) * (glm::dot(normal, particlesContainer[i].pos) + d)*normal;
 			particlesContainer[i].vel = particlesContainer[i].vel - (1 + coef) * (glm::dot(normal, particlesContainer[i].vel))*normal;
@@ -175,7 +179,7 @@ void PhysicsUpdate(float dt) {
 				particlesContainer[i].vel = glm::vec3(-3, 0, ((float)rand() / RAND_MAX) * 6.f - 3.f); //random
 				particlesContainer[i].life = 0;
 			}
-			else if (waterfall == false) {
+			else if (!waterfall) {
 			particlesContainer[i].pos = glm::vec3(0,1,0);
 			particlesContainer[i].vel = glm::vec3(((float)rand() / RAND_MAX) * 6.f - 3.f, (10 + ((float)rand() / RAND_MAX) * 6.f - 3.f), ((float)rand() / RAND_MAX) * 6.f - 3.f); //random
 			particlesContainer[i].life = 0;
